12dda120ce644cf57935de63d2fcba23612ee316
MeGGi <megilangr1@gmail.com>
2022-08-09 09:34:03 +0700

    Fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,7 +41,6 @@ class AuthController extends Controller
 
         if (auth()->attempt($credentials)) {
             $checkRoles = $user->getRoleNames()->toArray();
-            dd($checkRoles);
             if (in_array('Owner', $checkRoles) || in_array('Administrator', $checkRoles)) {
                 return redirect()->route('backend.main');
             } else {
